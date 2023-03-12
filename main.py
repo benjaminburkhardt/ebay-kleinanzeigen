b608c9c2a480577488e2e444ece1706f07617f25
Benjamin Burkhardt <benjamin.burkhardt@t-systems.com>
2023-03-12 12:28:29 +0100

    filter non womos and cleanup

--- main.py
+++ main.py
@@ -25,19 +25,17 @@ last_items = {}
 INTERVAL_MINS = 10
 
 logger = utils.get_logger()
-
-# Instantiate the parser
 parser = argparse.ArgumentParser(description='Telegram bot to notify about new articles')
 
-
 class Item:
-    def __init__(self, title, price, torg, url, date, image):
+    def __init__(self, title, price, torg, url, date, image, tag):
         self.title = title
         self.price = price
         self.torg = torg
         self.url = 'https://www.ebay-kleinanzeigen.de' + url
         self.date = date
         self.image = image
+        self.tag = tag
 
     def __repr__(self):
         return f'{self.title} \n {self.price}€ \n {self.date}'
@@ -46,12 +44,16 @@ class Item:
         result = f'{self.title} \n{self.price}.000€'
         if self.torg:
             result += ' VB'
-        result += f'\n{self.date}\n'
-
+        #result += f'\n{self.date}\n'
+        result += f'\n{self.tag}\n'
         result += self.url
         result += '\n'
         return result
 
+def is_womo(item: Item):
+    if "Wohnwagen" in item.title:
+        return False
+    return True
 
 def get_items_per_url(url):
     log = utils.get_logger()
@@ -73,7 +75,17 @@ def get_items_per_url(url):
             # This is ad block, skip
             continue
 
+        # Checking if article is a "TOP" article/ad, currently evaluating, but probably identifying by price is enough
+        #        try:
+        #            test = re.findall('icon icon-smaller icon-feature-topad.*?</i>(.*?)</', item, re.S)[0].strip()
+        #            log.info("werbung")
+        #            continue
+        #        except Exception as e:
+        #            # regular entry, do nothing
+
         soup = BeautifulSoup(item, 'html.parser')
+
+        # Name
         soup_result = soup.find_all("a", {"class": 'ellipsis'})
         if len(soup_result) > 0:
             url = soup_result[0]['href']
@@ -81,6 +93,7 @@ def get_items_per_url(url):
         else:
             continue
 
+        #Price
         price_line = re.findall('aditem-main--middle--price.*?>(.*?)</p>', item, re.S)
         if len(price_line) > 0:
             price_line = price_line[0]
@@ -90,15 +103,8 @@ def get_items_per_url(url):
         price = None
         if prices := re.findall(r'\d+', price_line, re.S):
             price = int(prices[0])
-# Checking if article is a "TOP" article/ad, currently evaluating, but probably identifiying by price is enough
-#        try:
-#            test = re.findall('icon icon-smaller icon-feature-topad.*?</i>(.*?)</', item, re.S)[0].strip()
-#            log.info("werbung")
-#            continue
-#        except Exception as e:
-#            # If there is no feattured OK  - it's some highlighted/"top" item
-#            log.info("normlaler eintrag")
 
+        # Date
         date = datetime.datetime.now()
         try:
             date = re.findall('icon icon-small icon-calendar-open.*?</i>(.*?)</', item, re.S)[0].strip()
@@ -111,13 +117,20 @@ def get_items_per_url(url):
             log.info("Skipped ad..")
             continue
 
+        # Tag
+        try:
+            tag = re.findall('simpletag tag-small">(.*?)</', item, re.S)[0].strip()
+        except Exception as e:
+            log.info("Could not get tag...")
+
+        # Image
         try:
             image = re.findall('imgsrc="(.*?)"', item, re.S)[0].strip()
         except Exception as e:
             logger.error(f'No image\n\t{item}')
             continue
         log.info("Found & added URL: " + url)
-        items.append(Item(name, price, torg, url, date, image))
+        items.append(Item(name, price, torg, url, date, image, tag))
     return items
 
 
@@ -132,7 +145,6 @@ def error(update, context):
     """Log Errors caused by Updates."""
     print('Update "%s" caused error "%s"', update, context.error)
 
-
 def echo(update: Update, context):
     msg: Message = update.message
 
@@ -144,19 +156,20 @@ def echo(update: Update, context):
 
     if chat_id not in last_items:
         # Nothing here, schedule
-        print(INTERVAL_MINS)
         scheduler.add_job(echo, trigger='interval', args=(update, context), minutes=INTERVAL_MINS, id=str(chat_id))
         log.info('Scheduled job')
         last_items[chat_id] = {'last_item': None, 'url': url}
 
-    log.info("Get items")
     items = get_items_per_url(url)
     for item in items:
         if chat_id in last_items and item.url == last_items[chat_id]['last_item']:
-            log.info('Last item, breaking the loop...')
+            log.info('Found last item, breaking the loop...')
             break
-        msg.reply_text(str(item))
-        # update.message.reply_photo(item.image)
+        if is_womo(item):
+            msg.reply_text(str(item))
+            # update.message.reply_photo(item.image)
+        else:
+            log.info("Skipped non womo: " + item.title)
     last_items[chat_id] = {'last_item': items[0].url, 'search_url': url}
     log.info(last_items)
 
@@ -165,21 +178,21 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    
+
     # Read arguments
     parser.add_argument('--token', type=ascii,
-                    help='Telegram token for bot')
+                        help='Telegram token for bot')
     parser.add_argument('--opt_minutes', type=int,
-                    help='Check ebay each x minutes')
+                        help='Check ebay each x minutes')
     args = parser.parse_args()
-    
+
     BOT_TOKEN = str(args.token.strip('\''))
     global INTERVAL_MINS
     if args.opt_minutes:
         INTERVAL_MINS = args.opt_minutes
 
     print("Checking each " + str(INTERVAL_MINS) + " min")
-    
+
     updater = Updater(bot=utils.get_bot(BOT_TOKEN), use_context=True)
     dp = updater.dispatcher
 
